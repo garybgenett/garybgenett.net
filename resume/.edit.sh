ff241a7165f421eeb391f32f06d368b915b94220
Gary B. Genett <me@garybgenett.net>
2020-01-13 16:04:06 -0800

    [git-save :: 2020-01-13T16:04:06-08:00]

--- resume/.edit.sh
+++ resume/.edit.sh
@@ -1,14 +1,75 @@
 #!/usr/bin/env bash
 source ${HOME}/.bashrc
 
-cd $(dirname ${_SELF})
-chown -R plastic ./
-view resume.md resume-cover.md
-vdiff $(ls resume.md.BAK-[0-9].[0-9]		| tail -n2 | head -n1) resume.md
-vdiff $(ls resume.md.BAK-[0-9].[0-9]-cover.md	| tail -n2 | head -n1) resume-cover.md
+RESUME="resume.md"
+_COVER="resume-cover.md"
 
-cd ../
-git-perms root
+declare DIR="$(dirname ${_SELF})"
+declare BAS="${DIR}/${RESUME}.BAK"
+declare PST=""
+declare CVR="-cover.md"
+
+declare RES_OLD="$(${LS} ${BAS}-[0-9].[0-9]		| tail -n2 | head -n1)"
+declare RES_NEW="$(${LS} ${BAS}-[0-9].[0-9]		| tail -n1)"
+declare RES_MIN="$(echo "${RES_NEW}"			| ${SED} "s|^.+([0-9])[.]([0-9]).*$|\2|g")"
+declare RES_MAJ="$(echo "${RES_NEW}"			| ${SED} "s|^.+([0-9])[.]([0-9]).*$|\1|g")"
+
+declare CVR_OLD="$(${LS} ${BAS}-[0-9].[0-9]${CVR}	| tail -n2 | head -n1)"
+declare CVR_NEW="$(${LS} ${BAS}-[0-9].[0-9]${CVR}	| tail -n1)"
+declare CVR_MIN="$(echo "${CVR_NEW}"			| ${SED} "s|^.+([0-9])[.]([0-9]).*$|\2|g")"
+declare CVR_MAJ="$(echo "${CVR_NEW}"			| ${SED} "s|^.+([0-9])[.]([0-9]).*$|\1|g")"
+
+declare NEW_MIN="${RES_MAJ}.$((${RES_MIN}+1))"
+declare NEW_MAJ="$((${RES_MAJ}+1)).0"
+
+echo -en "
+OLD: ${RES_OLD}
+NEW: ${RES_NEW}
+VER: ${RES_MAJ}.${RES_MIN}
+
+OLD: ${CVR_OLD}
+NEW: ${CVR_NEW}
+VER: ${CVR_MAJ}.${CVR_MIN}
+
+MIN: ${NEW_MIN}
+MAJ: ${NEW_MAJ}
+\n"
+
+chown -R plastic ${DIR}
+
+function update {
+	declare OLD=
+	declare NEW=
+	declare EXT=
+	declare VER=
+	if [[ ${1} == -r ]]; then OLD="${RES_NEW}"; NEW="${DIR}/${RESUME}"; EXT="${RES}"; shift; fi
+	if [[ ${1} == -c ]]; then OLD="${CVR_NEW}"; NEW="${DIR}/${_COVER}"; EXT="${CVR}"; shift; fi
+	if [[ ${1} == -n ]]; then VER="${NEW_MIN}"; shift; fi
+	if [[ ${1} == -m ]]; then VER="${NEW_MAJ}"; shift; fi
+	echo "${RSYNC_U}	${OLD}			${BAS}-${VER}${EXT}"
+	echo "${LN}		${BAS}-${VER}${EXT}	${NEW}"
+}
+
+if [[ -n ${1} ]]; then
+	if [[ ${1} == -n ]]; then shift
+		update -r -n
+		if [[ ${1} == -c ]]; then shift
+			update -c -n
+		fi
+	fi
+	if [[ ${1} == -m ]]; then shift
+		update -r -m
+		if [[ ${1} == -c ]]; then shift
+			update -c -m
+		fi
+	fi
+fi
+
+view ${RES_NEW} ${CVR_NEW}
+vdiff ${RES_OLD} ${RES_NEW}
+vdiff ${CVR_OLD} ${CVR_NEW}
+
+(cd $(dirname ${DIR}) && git-perms root)
 
 exit 0
 # end of file
